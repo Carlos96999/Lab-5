59f07aea14f7397c739fc4da2d22467932727080
Carlos96999 <allblacksc@hotmail.com>
2020-09-18 20:18:25 -0500

    Problema en la variable average en el xml

--- ciclos/src/main/java/BackingBean/backingBean.java
+++
@@ -1,5 +0,0 @@
-package BackingBean;
-
-public class backingBean {
-
-}
---
+++ ciclos/src/main/java/BackingBean/calculadoraBean.java
@@ -0,0 +1,141 @@
+package main.java.BackingBean;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean(name = "calculadoraBean")
+@ApplicationScoped
+public class calculadoraBean {
+
+	private ArrayList<Float> data;
+	public double average;
+	private double desviation;
+	private double variance;
+	private double mode;
+
+	public calculadoraBean()
+	{
+		average = 0.0;
+		desviation = 0.0;
+		variance = 0.0;
+		mode = 0.0;
+	}
+	
+	public double calculateMean(ArrayList<Float> Mean)
+	{
+		for(int i=0; i<Mean.size(); i++)
+		{
+			average += Mean.get(i);
+		}
+		average = average/2;
+		return average;
+	}
+	
+	public double calculateVariance(ArrayList<Float> variance)
+	{
+		calculateMean(variance);
+		
+		for(int i=0; i<variance.size(); i++)
+		{
+			this.variance += Math.pow(variance.get(i)-average, 2);
+		}
+		this.variance /= variance.size();
+		return this.variance;
+	}
+	
+	public double calculateStandardDeviation(ArrayList<Float> standardDeviation)
+	{
+		desviation = calculateVariance(standardDeviation);
+		desviation = Math.sqrt(desviation);
+		return desviation;
+	}
+	
+	public double calculateMode(ArrayList<String> mode)
+	{
+		HashMap<String, Integer> map = new HashMap<>();
+		
+		for (int i = 0; i<mode.size(); i++) 
+		{
+			//Float number = mode.get(i);
+			String number = mode.get(i);
+			
+			if (map.containsKey(number)) 
+			{
+				map.put(number, map.get(number) + 1);
+			} 
+			else 
+			{
+				map.put(number, 1);
+			}
+		}
+		
+		String moda = "";
+		int mayor = 0;
+		
+		for (HashMap.Entry<String, Integer> entry : map.entrySet())
+		{
+			if (entry.getValue() > mayor) 
+			{
+				mayor = entry.getValue();
+				moda = entry.getKey();
+			}
+		}
+		return 1.0;
+	}
+	
+	public void restart()
+	{
+		average = 0.0;
+		desviation = 0.0;
+		variance = 0.0;
+		mode = 0.0;
+	}
+	
+	/*
+	 * Conjunto de Get's
+	 */
+	public ArrayList<Float> getData()
+	{
+		return data;
+	}
+	
+	public double getMean()
+	{
+		return average;
+	}
+	
+	public double getDesviation()
+	{
+		return desviation;
+	}
+	
+	public double getVariance()
+	{
+		return variance;
+	}
+	
+	public double getMode()
+	{
+		return mode;
+	}
+	
+	public int getCount()
+	{
+		return data.size();
+	}
+	
+	/*
+	 * Conjunto de Set's
+	 */
+	public void setData(ArrayList<Float> data)
+	{
+		this.data = data;
+	}
+	
+	public void main(String[] args)
+	{
+		calculadoraBean calculate = new calculadoraBean();
+	}
+}
